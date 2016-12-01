b9eabbaf992d06445e32212bca2fb05f034bf8cc
Abigail Watson <awatson1978@gmail.com>
2016-12-01 12:15:22 -0600

    bump deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-clinical-impression',
-  version: '1.2.1',
+  version: '1.2.2',
   summary: 'HL7 FHIR Resource - Clinical Impression',
   git: 'https://github.com/clinical-meteor/hl7-resource-clinical-impression',
   documentation: 'README.md'
@@ -17,12 +17,13 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.5.6');
+  api.use('clinical:hl7-resource-datatypes@0.6.0');
 
   api.addFiles('lib/hl7-resource-clinical-impression.js');
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
 
-  api.export('ClinicalImpressionSchema');
+  api.export('ClinicalImpression');
   api.export('ClinicalImpressions');
+  api.export('ClinicalImpressionSchema');
 });
