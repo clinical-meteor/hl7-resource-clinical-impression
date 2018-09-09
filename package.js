d240ee201b9ffcdcc0267e7c818b3a0f0c9a02af
Abigail Watson <abigailwatson@Abigails-Mac-mini-379644.local>
2018-09-09 01:46:32 -0500

    QA pass

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-clinical-impression',
-  version: '2.0.0',
+  version: '2.0.1',
   summary: 'HL7 FHIR Resource - Clinical Impression',
   git: 'https://github.com/clinical-meteor/hl7-resource-clinical-impression',
   documentation: 'README.md'
